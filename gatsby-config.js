fdf83b07bb621757812e7cc3230a15c749b946e3
Martin M <mtendotnet@users.noreply.github.com>
2018-10-13 10:51:24 +0100

    Initial commit - from gatsby default starter

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter',
+        short_name: 'gatsby starter',
+        start_url: '/',
+        background_color: '#FFFFFF',
+        theme_color: '#258',
+        display: 'minimal-ui',
+        icon: 'src/images/m-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Home Page</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+  </Layout>
+)
+
+export default IndexPage
